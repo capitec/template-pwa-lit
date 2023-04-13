d8725daed421186e803a556238f5e05f5741f47d
BOTLANNER <16349308+BOTLANNER@users.noreply.github.com>
2023-04-13 09:02:37 +0200

    Added named slot example

--- src/modules/module-b/ViewThree.ts
+++ src/modules/module-b/ViewThree.ts
@@ -9,6 +9,7 @@ import '@capitec/omni-components/button';
 import '@capitec/omni-components/check';
 import '@capitec/omni-components/email-field';
 import '@capitec/omni-components/hyperlink';
+import '@capitec/omni-components/icon';
 import '@capitec/omni-components/label';
 import '@capitec/omni-components/pin-field';
 import '@capitec/omni-components/radio';
@@ -56,6 +57,11 @@ export class ViewThree extends ViewBase {
                 margin-top: 15px;
              }
 
+             .suffix-slot {
+               margin-right: 10px;
+               color: var(--global-font-color);
+             }
+
         `
     ];
 
@@ -68,8 +74,11 @@ export class ViewThree extends ViewBase {
                         id="form-email-field" 
                         label="Email field" 
                         hint="Enter a valid email address" 
-                        .value="${live(this._emailValue as string )}" 
+                        .value="${live(this._emailValue as string)}" 
                         @input="${(e: InputEvent) => this._emailFieldInput(e)}">
+                        <omni-icon size="medium" slot="suffix" class="suffix-slot">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="100%" height="100%"><path d="M22 3.25a.75.75 0 0 1 .75.75v16a.75.75 0 0 1-.75.75H2a.75.75 0 0 1-.75-.75V4A.75.75 0 0 1 2 3.25Zm-.75 3.19-8.82 6.174a.75.75 0 0 1-.76.06l-.1-.06-8.82-6.173V19.25h18.5V6.44Zm-.202-1.69H2.951L12 11.084l9.048-6.334Z"/></svg>
+                        </omni-icon>
                     </omni-email-field>
                     <omni-pin-field 
                         id="form-pin-field" 
@@ -83,7 +92,8 @@ export class ViewThree extends ViewBase {
                         <omni-radio label="Admin"></omni-radio>
                         <omni-radio label="Casual User" checked></omni-radio>
                     </omni-radio-group>
-                    <omni-check id="form-check" label="I agree all fields above are populated" @click="${(e: Event) => this._checkClicked(e)}"></omni-check>
+                    <omni-check id="form-check" label="I agree all fields above are populated" @click="${(e: Event) =>
+                        this._checkClicked(e)}"></omni-check>
                     <omni-button label="Submit" type="primary" @click="${() => this._formSubmitted()}"></omni-button>
                 </div>
             </div>    
@@ -115,11 +125,11 @@ export class ViewThree extends ViewBase {
             alert('Your form is valid');
         } else {
             this._formValid = false;
-            
+
             if (!emailField.value) {
                 emailField.error = 'Please enter an email value';
             }
-            
+
             if (!pinField.value) {
                 pinField.error = 'Please enter a pin value';
             }
